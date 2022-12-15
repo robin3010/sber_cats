eae54226ae3e9eb8578f4716153816bf7157d541
robin3010 <robin3010@gmail.com>
2022-12-15 21:05:29 +0300

    cat remove implemented; add cat image placeholder logic

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const USER = 'robin3010';
 const BASE_URL = `https://cats.petiteweb.dev/api/single/${USER}`;
 
-const $box = document.querySelector('[data-box]');
+const D_ATTR = {
+  BOX: 'box',
+  ACTION: 'action',
+  CAT_CARD: 'cat-card',
+};
+const ACTIONS = {
+  MORE: 'more',
+  EDIT: 'edit',
+  DELETE: 'delete',
+};
+
+const $box = document.querySelector(`[data-${D_ATTR.BOX}]`);
 
 const makeCatCard = (cat) => {
   let fav = 'regular';
   if (cat.favorite) {
     fav = 'solid';
   }
-  return `<div class="card">
-    <div class="card__img">
-      <img src="${cat.image}" alt="${cat.name}" width="250px">
+
+  let imgPHClass = ' card__img-placeholder';
+  let img = './img/img-placeholder.png';
+  if (cat.image) {
+    img = cat.image;
+    imgPHClass = '';
+  }
+
+  return `<div data-${D_ATTR.CAT_CARD} data-id="${cat.id}" class="card">
+    <div class="card__img${imgPHClass}">
+      <img src="${img}" alt="${cat.name}">
     </div>
     <div class="card__title">
       <h2>${cat.name.toUpperCase()}</h2>
@@ -20,9 +39,9 @@ const makeCatCard = (cat) => {
       <p>${cat.description}</p>
     </div>
     <div class="card__buttons">
-      <button class="more">More...</button>
-      <button class="edit">Edit</button>
-      <button class="delete">Delete</button>
+      <button data-${D_ATTR.ACTION}="${ACTIONS.MORE}">More...</button>
+      <button data-${D_ATTR.ACTION}="${ACTIONS.EDIT}">Edit</button>
+      <button data-${D_ATTR.ACTION}="${ACTIONS.DELETE}">Delete</button>
     </div>
   </div>
     `;
@@ -33,3 +52,15 @@ fetch(`${BASE_URL}/show/`)
   .then((allCats) => {
     $box.insertAdjacentHTML('afterbegin', allCats.map((el) => makeCatCard(el)).join(''));
   });
+
+$box.addEventListener('click', (e) => {
+  if (e.target.dataset[D_ATTR.ACTION] === ACTIONS.DELETE) {
+    const $catCard = e.target.closest(`[data-${D_ATTR.CAT_CARD}]`);
+    fetch(`${BASE_URL}/delete/${$catCard.dataset.id}`, { method: 'DELETE' })
+      .then((response) => {
+        if (response.status === 200) {
+          $catCard.remove();
+        }
+      });
+  }
+});
