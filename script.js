749cafac05358c29b1533d3ac81040ae9a8aa856
robin3010 <robin3010@gmail.com>
2022-12-17 14:39:30 +0300

    modal cat-info card done

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ const USER = 'robin3010';
 const BASE_URL = `https://cats.petiteweb.dev/api/single/${USER}`;
 
 const D_ATTR = {
-  BOX: 'box',
-  ACTION: 'action',
-  CARD: 'cat-card',
-  MODAL: 'modal',
+  CONTAINER: 'data-container',
+  ACTION: 'data-action',
+  CARD: 'data-cat-card',
+  MODAL: 'data-modal',
+  MODAL_CONTAINER: 'data-modalcontainer',
 };
+
+// копия D_ATTR без 'data-' у ключей, для обращения через dataset
+const DATASET = structuredClone(D_ATTR);
+for (const k in DATASET) {
+  if (DATASET) {
+    DATASET[k] = DATASET[k].substring(5);
+  }
+}
+
 const ACTIONS = {
   MORE: 'more',
   EDIT: 'edit',
@@ -18,7 +28,9 @@ const MODAL = {
   EDIT: 'edit-form',
 };
 
-const $box = document.querySelector(`[data-${D_ATTR.BOX}]`);
+const $container = document.querySelector(`[${D_ATTR.CONTAINER}]`);
+const $modalContainer = document.querySelector(`[${D_ATTR.MODAL_CONTAINER}]`);
+const $modalMore = document.querySelector(`[${D_ATTR.MODAL}="${MODAL.MORE}"]`);
 
 const catCardHTML = (cat) => {
   let fav = 'regular';
@@ -33,43 +45,28 @@ const catCardHTML = (cat) => {
     imgPHClass = '';
   }
 
-  return `<div data-${D_ATTR.CARD} data-id="${cat.id}" class="card">
+  return `
+  <div ${D_ATTR.CARD} data-id="${cat.id}" class="card">
     <div class="card__img${imgPHClass}">
       <img src="${img}" alt="${cat.name}">
     </div>
-    <div class="card__title">
-      <h2>${cat.name.toUpperCase()}</h2>
-      <button class="card-favorite"><i class="fa-${fav} fa-heart"></i></button>
-    </div>
-    <div class="card__description">
-      <p>${cat.description}</p>
-    </div>
-    <div class="card__buttons">
-      <button data-${D_ATTR.ACTION}="${ACTIONS.MORE}">More...</button>
-      <button data-${D_ATTR.ACTION}="${ACTIONS.EDIT}">Edit</button>
-      <button data-${D_ATTR.ACTION}="${ACTIONS.DELETE}">Delete</button>
+    <div class="card__content">
+      <div class="card__title">
+        <h2>${cat.name.toUpperCase()}</h2>
+        <button class="card-favorite"><i class="fa-${fav} fa-heart"></i></button>
+      </div>
+      <div class="card__description">
+        <p>${cat.description}</p>
+      </div>
+      <div class="card__buttons">
+        <button ${D_ATTR.ACTION}="${ACTIONS.MORE}">More...</button>
+        <button ${D_ATTR.ACTION}="${ACTIONS.EDIT}">Edit</button>
+        <button ${D_ATTR.ACTION}="${ACTIONS.DELETE}">Delete</button>
+      </div>
     </div>
   </div>
     `;
 };
-
-fetch(`${BASE_URL}/show/`)
-  .then((response) => response.json())
-  .then((allCats) => {
-    $box.insertAdjacentHTML('afterbegin', allCats.map((el) => catCardHTML(el)).join(''));
-  });
-
-$box.addEventListener('click', (e) => {
-  if (e.target.dataset[D_ATTR.ACTION] === ACTIONS.DELETE) {
-    const $catCard = e.target.closest(`[data-${D_ATTR.CARD}]`);
-    fetch(`${BASE_URL}/delete/${$catCard.dataset.id}`, { method: 'DELETE' })
-      .then((response) => {
-        if (response.status === 200) {
-          $catCard.remove();
-        }
-      });
-  }
-});
 
 const cutCatCardBts = (catCard) => {
   const re = /\s*<div class=.card__buttons.*$/gs;
@@ -84,26 +81,64 @@ const catModalCardHTML = (cat) => {
   }
 
   return `
-  <div class="card__age">
-    Age: ${cat.age}
+    <div class="card__age">
+      Age: ${cat.age}
+    </div>
+    <div class="card__rate">
+      Rating: ${rate}
+    </div>
   </div>
-  <div class="card__rate">
-    Rating: ${rate}
-  </div>
-</div>`;
+</div>
+  `;
 };
 
-const $modal = document.querySelector(`[data-${D_ATTR.MODAL}="${MODAL.MORE}"]`);
+fetch(`${BASE_URL}/show/`)
+  .then((response) => response.json())
+  .then((allCats) => {
+    $container.insertAdjacentHTML('afterbegin', allCats.map((el) => catCardHTML(el)).join(''));
+  });
 
-$box.addEventListener('click', (e) => {
-  if (e.target.dataset[D_ATTR.ACTION] === ACTIONS.MORE) {
-    const $catCard = e.target.closest(`[data-${D_ATTR.CARD}]`);
-    const cutCatCardHTML = cutCatCardBts($catCard);
+const toggleModalContainer = () => $modalContainer.classList.toggle('hidden');
 
-    (async function catInfo() {
-      const getCatCardByID = await fetch(`${BASE_URL}/show/${$catCard.dataset.id}`);
-      const catJSON = await getCatCardByID.json();
-      return $modal.insertAdjacentHTML('afterbegin', await cutCatCardHTML + catModalCardHTML(catJSON));
-    }());
+const closeModal = (e) => {
+  if (e.target === $modalContainer) {
+    $modalMore.replaceChildren();
+    toggleModalContainer();
+    $modalContainer.removeEventListener('click', closeModal);
+  }
+};
+
+$container.addEventListener('click', (e) => {
+  if (e.target !== $container) {
+    const $catCard = e.target.closest(`[${D_ATTR.CARD}]`);
+    const catId = $catCard.dataset.id;
+
+    switch (e.target.dataset[DATASET.ACTION]) {
+      case ACTIONS.DELETE:
+        fetch(`${BASE_URL}/delete/${catId}`, { method: 'DELETE' })
+          .then((response) => {
+            if (response.status === 200) {
+              return $catCard.remove();
+            }
+            throw new Error(`Не удалось удалить кота с id = ${catId}`);
+          });
+        break;
+
+      case ACTIONS.MORE:
+        const cutCatCardHTML = cutCatCardBts($catCard);
+
+        (async function catInfo() {
+          const getCatCardByID = await fetch(`${BASE_URL}/show/${catId}`);
+          const catJSON = await getCatCardByID.json();
+          return $modalMore.insertAdjacentHTML('afterbegin', await cutCatCardHTML + catModalCardHTML(catJSON));
+        }());
+        toggleModalContainer();
+
+        $modalContainer.addEventListener('click', closeModal);
+        break;
+
+      default:
+        break;
+    }
   }
 });
