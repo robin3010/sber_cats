160340da64d37e4240576ff2c1fe074d8bd0ce5f
robin3010 <robin3010@gmail.com>
2022-12-20 03:59:06 +0300

    objective_2 done! added cat add functional, styles, different scenarios of closing modal

--- script.js
+++ script.js
@@ -18,19 +18,22 @@ for (const k in DATASET) {
 }
 
 const ACTIONS = {
+  ADD: 'add',
   DETAIL: 'detail',
   EDIT: 'edit',
   DELETE: 'delete',
 };
 const MODAL = {
-  ADD: 'add-form',
+  ADD: 'form-add',
+  EDIT: 'form-edit',
   DETAIL: 'detail-info',
-  EDIT: 'edit-form',
 };
 
 const $container = document.querySelector(`[${D_ATTR.CONTAINER}]`);
-const $modalContainer = document.querySelector(`[${D_ATTR.MODAL_CONTAINER}]`);
+// const $modalContainer = document.querySelector(`[${D_ATTR.MODAL_CONTAINER}]`);
 const $modalDetail = document.querySelector(`[${D_ATTR.MODAL}="${MODAL.DETAIL}"]`);
+const $modalAdd = document.querySelector(`[${D_ATTR.MODAL}="${MODAL.ADD}"]`);
+const $formTemplate = document.querySelector('[data-form-template]');
 
 const catCardHTML = (cat) => {
   let fav = 'regular';
@@ -92,50 +95,89 @@ const catModalCardHTML = (cat) => {
   `;
 };
 
-// get all cats
+const formatFormData = (data) => ({
+  ...data,
+  id: +data.id,
+  age: +data.age,
+  rate: +data.rate,
+  favorite: !!data.favorite,
+});
+
+// получение и отображение карточек всех котов
 fetch(`${BASE_URL}/show/`)
   .then((response) => response.json())
   .then((allCats) => {
     $container.insertAdjacentHTML('afterbegin', allCats.map((el) => catCardHTML(el)).join(''));
   });
 
-const toggleModalContainer = () => $modalContainer.classList.toggle('hidden');
-
-const closeModal = (e) => {
-  if (e.target === $modalContainer) {
-    $modalDetail.replaceChildren();
-    toggleModalContainer();
-    $modalContainer.removeEventListener('click', closeModal);
-  }
-};
-
+// удаление кота
 function deleteHandler(card, id) {
   fetch(`${BASE_URL}/delete/${id}`, { method: 'DELETE' })
     .then((response) => {
       if (response.status === 200) {
         return card.remove();
       }
-      throw new Error(`Не удалось удалить кота с id = ${id}`);
-    });
+      throw Error(`Не удалось удалить кота с id = ${id}`);
+    }).catch(alert);
 }
 
+// добавление кота (отправка формы на сервер)
+function SubmitNewCatHandler(submitEvent) {
+  const getFormData = Object.fromEntries(new FormData(submitEvent.target).entries());
+  const formattedFormData = formatFormData(getFormData);
+
+  (async function addCat() {
+    const addNewCat = await fetch(`${BASE_URL}/add/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formattedFormData),
+    });
+    try {
+      if (addNewCat.status === 200) {
+        return $container.insertAdjacentHTML('afterbegin', catCardHTML(formattedFormData));
+      }
+      throw Error('Ошибка при добавлении кота');
+    } catch (err) {
+      alert(err);
+    }
+  }());
+}
+
+// вкл/выкл отображения контейнера модального окна
+const toggleModal = (modal) => modal.classList.toggle('hidden');
+
+// показать подробную карточку кота в модальном окне
 function detailHandler(card, id) {
   const cutCatCardHTML = cutCatCardBts(card);
 
   (async function catInfo() {
-    const getCatCardByID = await fetch(`${BASE_URL}/show/${id}`);
-    const catJSON = await getCatCardByID.json();
+    const getCatDataByID = await fetch(`${BASE_URL}/show/${id}`);
+    const catJSON = await getCatDataByID.json();
     return $modalDetail.insertAdjacentHTML('afterbegin', await cutCatCardHTML + catModalCardHTML(catJSON));
   }());
-  toggleModalContainer();
+  toggleModal($modalDetail);
 
-  $modalContainer.addEventListener('click', closeModal);
+  // eslint-disable-next-line no-use-before-define
+  flushModal($modalDetail);
 }
 
+// показать форму добавления кота
+const addHandler = () => {
+  const addNewCatForm = $formTemplate.content.cloneNode(true);
+  $modalAdd.appendChild(addNewCatForm);
+  toggleModal($modalAdd);
+
+  // eslint-disable-next-line no-use-before-define
+  flushModal($modalAdd);
+};
+
+// обработка кликов на кнопки
 function buttonsHandler(e) {
-  if (e.target !== $container) {
-    const $catCard = e.target.closest(`[${D_ATTR.CARD}]`);
-    const catId = $catCard.dataset.id;
+  if (e.target.dataset[DATASET.ACTION]) {
+    const $catCard = !e.target.dataset.action.endsWith('add') ? e.target.closest(`[${D_ATTR.CARD}]`) : '';
+    const catId = $catCard ? $catCard.dataset.id : '';
 
     switch (e.target.dataset[DATASET.ACTION]) {
       case ACTIONS.DELETE:
@@ -143,7 +185,13 @@ function buttonsHandler(e) {
         break;
 
       case ACTIONS.DETAIL:
+        document.removeEventListener('click', buttonsHandler);
         detailHandler($catCard, catId);
+        break;
+
+      case ACTIONS.ADD:
+        document.removeEventListener('click', buttonsHandler);
+        addHandler();
         break;
 
       default:
@@ -152,4 +200,51 @@ function buttonsHandler(e) {
   }
 }
 
-$container.addEventListener('click', buttonsHandler);
+document.addEventListener('click', buttonsHandler);
+
+// сценарии закрытия модальных окон
+function flushModal(modal) {
+  // по нажатию Escape
+  function closeByEsc(e) {
+    if (e.key === 'Escape') {
+      modal.replaceChildren();
+      toggleModal(modal);
+      // eslint-disable-next-line no-use-before-define
+      removeListeners();
+    }
+  }
+
+  // по клику на затемненную область
+  function closeByClick(e) {
+    if (e.target.dataset.modal) {
+      e.target.replaceChildren();
+      toggleModal(e.target);
+      // eslint-disable-next-line no-use-before-define
+      removeListeners();
+    }
+  }
+
+  // при отправке формы
+  function closeBySubmit(e) {
+    if (e.target === document.forms['add' || 'edit']) {
+      e.preventDefault();
+      SubmitNewCatHandler(e);
+      modal.replaceChildren();
+      toggleModal(modal);
+      // eslint-disable-next-line no-use-before-define
+      removeListeners();
+    }
+  }
+
+  function removeListeners() {
+    document.addEventListener('click', buttonsHandler);
+    document.removeEventListener('keydown', closeByEsc);
+    modal.removeEventListener('mousedown', closeByClick);
+    modal.removeEventListener('submit', closeBySubmit);
+  }
+
+  document.removeEventListener('click', buttonsHandler);
+  document.addEventListener('keydown', closeByEsc);
+  modal.addEventListener('mousedown', closeByClick);
+  modal.addEventListener('submit', closeBySubmit);
+}
